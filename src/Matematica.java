8ad621e360bcbcc4f6ad81138077b721b6c08595
carloseduardoxp <carloseduardoxpto@gmail.com>
2018-09-26 22:36:20 -0300

    inclusao de casos de teste e bigdecimal

--- src/Matematica.java
+++ src/Matematica.java
@@ -1,14 +1,15 @@
+import java.math.BigDecimal;
 
 public class Matematica {
 	
-	public int fatorial(int n) throws MatematicaException {
+	public BigDecimal fatorial(int n) throws MatematicaException {
 		if (n < 0) {
 			throw new MatematicaException("Não aceita menor do que 0 "+n);
 		}
 		if (n == 0 || n == 1) {
-			return 1;
+			return BigDecimal.ONE;
 		}
-		return n * fatorial(n-1);
+		return BigDecimal.valueOf(n).multiply(fatorial(n-1));
 	}
 
 }
--- test/MatematicaTest.java
+++ test/MatematicaTest.java
@@ -1,27 +1,58 @@
+import java.math.BigDecimal;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MatematicaTest {
 	
+	private Matematica matematica; 
+	
+	@Before
+	public void init() {
+		matematica = new Matematica();
+	}
+	
 	@Test
 	public void testaFatorialZero() throws MatematicaException {
-		Matematica matematica = new Matematica();
-		int resposta = matematica.fatorial(0);
-		Assert.assertEquals(resposta,1);
+		BigDecimal resposta = matematica.fatorial(0);
+		Assert.assertEquals(resposta,BigDecimal.ONE);
 	}
 	
 	@Test
 	public void testaFatorialUm() throws MatematicaException {
-		Matematica matematica = new Matematica();
-		int resposta = matematica.fatorial(1);
-		Assert.assertEquals(resposta,1);
+		BigDecimal resposta = matematica.fatorial(1);
+		Assert.assertEquals(resposta,BigDecimal.ONE);
 	}
 
 
 	@Test(expected=MatematicaException.class)
 	public void testaFatorialMenorZero() throws MatematicaException {
-		Matematica matematica = new Matematica();
 		matematica.fatorial(-5);
+	}
+	
+	@Test
+	public void testaFatorialCinco() throws MatematicaException {
+		BigDecimal resposta = matematica.fatorial(5);
+		Assert.assertEquals(resposta,new BigDecimal(120));
+	}
+	
+	@Test
+	public void testaFatorialDoze() throws MatematicaException {
+		BigDecimal resposta = matematica.fatorial(12);
+		Assert.assertEquals(resposta,new BigDecimal(479001600));
+	}
+	
+	@Test
+	public void testaFatorialTreze() throws MatematicaException {
+		BigDecimal resposta = matematica.fatorial(13);
+		Assert.assertEquals(resposta,new BigDecimal(6227020800l));
+	}
+	
+	@Test
+	public void testaFatorialCem() throws MatematicaException {
+		BigDecimal resposta = matematica.fatorial(100);
+		Assert.assertEquals(resposta,new BigDecimal(6227020800l));
 	}
 
 }
